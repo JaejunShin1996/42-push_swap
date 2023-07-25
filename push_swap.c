762bc6d93093c68c0ac4440573159cbec182f538
Jaejun Shin <jaeshinkorean@gmail.com>
2023-07-25 10:31:27 +0930

    save

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jaeshin <jaeshin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 16:54:00 by jaeshin           #+#    #+#             */
-/*   Updated: 2023/07/24 23:25:18 by jaeshin          ###   ########.fr       */
+/*   Updated: 2023/07/25 10:08:20 by jaeshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,32 +46,36 @@ int	word_count(char	*argv[])
 	return (i);
 }
 
-t_stack	stack_numbers(char *argv[])
+t_stack	stack_numbers(char *argv[], char c)
 {
 	t_stack	stack;
-	char	**temp;
-	int		*nums;
 	int		i;
 
 	i = 0;
-	temp = ft_split(*argv, ' ');
-	nums = (int	*)malloc(word_count(argv) * sizeof(int));
-	while (temp[i++])
-		nums[i] = ft_atoi(temp[i]);
-	stack.numbers = nums;
-	stack.top = nums[--i];
+	stack.numbers = (int *)malloc(word_count(argv) * sizeof(int));
+	while (argv[i] && c == 'a')
+	{
+		stack.numbers[i] = ft_atoi(argv[i]);
+		i++;
+	}
+	if (c == 'a')
+		stack.top = stack.numbers[--i];
+	else
+		stack.top = 0;
 	return (stack);
 }
 
 int	main(int argc, char *argv[])
 {
 	t_stack	stack_a;
-	//t_stack	stack_b;
+	t_stack	stack_b;
+
 	if (argc < 1)
 		return (0);
 	if (!check_validity(++argv) || argc < 1)
 		return (0);
-	stack_a = stack_numbers(argv);
-	printf("%d\n", stack_a.top);
+	stack_a = stack_numbers(argv, 'a');
+	stack_b = stack_numbers(argv, 'b');
+	printf("%d, %d\n", stack_a.top, stack_b.top);
 	return (0);
 }
