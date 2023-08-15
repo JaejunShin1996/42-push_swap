a2b26e481f335e6ffe5595b43cdecf859c40a8e8
Jaejun Shin <jaeshinkorean@gmail.com>
2023-08-15 12:33:13 +0930

    working on optimisation

--- sources/sort_over_ten_3.c
+++ sources/sort_over_ten_3.c
@@ -1,34 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   operations_double.c                                :+:      :+:    :+:   */
+/*   sort_over_ten_3.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaeshin <jaeshin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/02 14:19:28 by jaeshin           #+#    #+#             */
-/*   Updated: 2023/08/15 11:37:38 by jaeshin          ###   ########.fr       */
+/*   Created: 2023/08/15 12:13:08 by jaeshin           #+#    #+#             */
+/*   Updated: 2023/08/15 12:32:50 by jaeshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	ss(t_stack *a, t_stack *b)
+void	opt_ss(t_stack *a, t_stack *b, int pivot)
 {
-	swap(a, 's');
-	swap(b, 's');
-	ft_putendl_fd("ss", 1);
-}
+	t_node	*a_head;
+	t_node	*b_head;
 
-void	rr(t_stack *a, t_stack *b)
-{
-	rotate(a, 'r');
-	rotate(b, 'r');
-	ft_putendl_fd("rr", 1);
-}
-
-void	rrr(t_stack *a, t_stack *b)
-{
-	reverse_rotate(a, 'r');
-	reverse_rotate(b, 'r');
-	ft_putendl_fd("rrr", 1);
+	a_head = a->head;
+	b_head = b->head;
+	if ((a_head->value > a_head->next->value) 
+		&& (b_head->value < b_head->next->value))
+	{
+		ss(a, b);
+	}
+	else if (a_head->value > a_head->next->value)
+	{
+		swap(a, 'a');
+	}
+	else if (b_head->value < b_head->next->value)
+	{
+		swap(b, 'b');
+	}
 }
